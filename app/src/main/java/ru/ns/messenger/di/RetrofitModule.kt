58bc5535ffad9c58c1ca195f485c70d2bf7c1c93
Sekunov Ilya <sekunovila@gmail.com>
2023-12-15 22:46:30 +0300

    remote ip add

--- app/src/main/java/ru/ns/messenger/di/RetrofitModule.kt
+++ app/src/main/java/ru/ns/messenger/di/RetrofitModule.kt
@@ -8,7 +8,8 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
-private const val BASE_URL = "http://10.0.2.2:8080/"
+//private const val BASE_URL_LOCAL = "http://10.0.2.2:8080/"
+private const val BASE_URL = "http://84.252.142.15:8080/"
 
 @Module
 @InstallIn(SingletonComponent::class)
